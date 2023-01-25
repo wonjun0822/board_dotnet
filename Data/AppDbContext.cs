1d945cebd1f0789d372322d29010eee9550b8602
wonjun0822 <juni2040@gmail.com>
2023-01-25 16:26:16 +0900

    초기 프로젝트 설정

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+
+using board_dotnet.Model;
+
+namespace board_dotnet.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public virtual DbSet<Article> Articles { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+
+        public AppDbContext() {}
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = @"Server=rds-mysql.cvewupsq1piq.ap-northeast-2.rds.amazonaws.com,3066;User Id=wonjun;Password=ekdud0822;Database=MYSQL_DB";
+
+            optionsBuilder.UseMySql(connection, ServerVersion.AutoDetect(connection));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken)) {
+            ChangeTracker.Entries().Where(x => x.State == EntityState.Added).ToList().ForEach(e => {
+                e.Property("createId").CurrentValue = "wonjun";
+                e.Property("createDate").CurrentValue = DateTime.Now;
+            });
+
+            ChangeTracker.Entries().Where(x => x.State == EntityState.Added || x.State == EntityState.Modified).ToList().ForEach(e => {
+                e.Property("updateId").CurrentValue = "wonjun";
+                e.Property("updateDate").CurrentValue = DateTime.Now;
+            });
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Service/ArticleService.cs
@@ -0,0 +1,64 @@
+using board_dotnet.Data;
+using board_dotnet.Model;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace board_dotnet.Service.ArticleService
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly AppDbContext _context;
+
+        public ArticleService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Article?> GetArticle(long id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+
+            if (article is null)
+                return null;
+
+            return article;
+        }
+
+        public async Task<List<Article>> AddArticle(Article article)
+        {
+            _context.Articles.Add(article);
+
+            await _context.SaveChangesAsync();
+
+            return await _context.Articles.();
+        }
+
+        public async Task<Article?> UpdateArticle(long id, Article request)
+        {
+            var article = await _context.Articles.FindAsync(id);
+
+            if (article is null)
+                return null;
+
+            article.title = request.title;
+            article.content = request.content;
+            article.hashTag = request.hashTag;
+            
+            await _context.SaveChangesAsync();
+
+            return article;
+        }
+
+        public async Task<List<Article>?> DeleteArticle(long id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+
+            if (article is null)
+                return null;
+                
+            _context.Articles.Remove(article);
+
+            return await _context.Articles.ToListAsync();
+        }
+    }
+}
