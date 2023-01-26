171c2c2b5bcd5827e6240cc4495592fde681a592
wonjun0822 <juni2040@gmail.com>
2023-01-26 10:57:41 +0900

    gitflow-feature-stash: #3
    Repository 경로 재 설정

---
+++ Repository/CommentRepository.cs
@@ -0,0 +1,63 @@
+using board_dotnet.Data;
+using board_dotnet.Interface;
+using board_dotnet.Model;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace board_dotnet.Repository
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CommentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Comment>?> GetComments(long articleId)
+        {
+            var comments = await _context.Comments.Where(e => EF.Property<long>(e, "articleId") == articleId).ToListAsync();
+
+            if (comments is null)
+                return null;
+
+            return comments;
+        }
+
+        public async Task<int> AddComment(long articleId, Comment comment)
+        {
+            _context.Entry(comment).Property("articleId").CurrentValue = articleId;
+
+            _context.Comments.Add(comment);
+
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<Comment?> UpdateComment(long commentId, Comment request)
+        {
+             var comment = await _context.Comments.FindAsync(commentId);
+
+            if (comment is null)
+                return null;
+
+            comment.comment = request.comment;
+            
+            await _context.SaveChangesAsync();
+
+            return comment;
+        }
+
+        public async Task<List<Comment>?> DeleteComment(long commentId)
+        {
+            var comment = await _context.Comments.FindAsync(commentId);
+
+            if (comment is null)
+                return null;
+                
+            _context.Comments.Remove(comment);
+
+            return await _context.Comments.ToListAsync();
+        }
+    }
+}
