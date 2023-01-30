cd4657886e60c6833155370e06d442dac58edcc6
wonjun0822 <juni2040@gmail.com>
2023-01-30 12:17:22 +0900

    #8-Swagger Exclude Schema Filter 적용
    Schema Filter 적용

--- Program.cs
+++ Program.cs
@@ -13,7 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.DocumentFilter<SwaggerIgnoreFilter>();
+});
 
 builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
@@ -73,4 +75,5 @@ app.UseCors(x => x
 
 app.MapControllers();
 
+app.MigrateDatabase();
 app.Run();
