ddd7242daa78fb484ac200acf16285f6dc86b289
wonjun0822 <juni2040@gmail.com>
2023-01-30 12:20:07 +0900

    #11-Migrations
    Migrations History 및 App 시작 시 Migrations 실행되도록 수정

---
+++ Migrations/20230130000331_ArticleToMemberRelationship.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace boarddotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArticleToMemberRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "createby",
+                table: "article",
+                type: "varchar(50)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_article_createby",
+                table: "article",
+                column: "createby");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_article_member_create_by",
+                table: "article",
+                column: "create_by",
+                principalTable: "member",
+                principalColumn: "member_id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_article_member_createby",
+                table: "article",
+                column: "createby",
+                principalTable: "member",
+                principalColumn: "member_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_article_member_create_by",
+                table: "article");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_article_member_createby",
+                table: "article");
+
+            migrationBuilder.DropIndex(
+                name: "IX_article_createby",
+                table: "article");
+
+            migrationBuilder.DropColumn(
+                name: "createby",
+                table: "article");
+        }
+    }
+}
