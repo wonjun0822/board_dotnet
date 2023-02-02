77355e52186edec155cd96b7124a44286fcac85e
wonjun0822 <juni2040@gmail.com>
2023-02-02 17:41:03 +0900

    Swagger Root 수정
    Swagger Root 수정

--- Program.cs
+++ Program.cs
@@ -69,7 +69,10 @@ var app = builder.Build();
 // }
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseResponseCompression();
 
